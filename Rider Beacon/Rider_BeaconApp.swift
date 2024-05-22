97558e24d5463b8ecac637845ee3e5c33fe992a9
Milan Ghimire <milan.ghimire@ensue.us>
2024-05-22 14:54:44 +0545

    Initial Commit

---
+++ Rider Beacon/Rider_BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rider_BeaconApp.swift
+//  Rider Beacon
+//
+//  Created by Ensue on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rider_BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
